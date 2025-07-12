a8f0703edd3b38ac87a881efc625476490cbaf15
Arthur Navarro <arthur6393@fito.br>
2025-07-11 22:46:29 -0300

    Preparação para integração do design kit e dark mode

--- TCC/settings.py
+++ TCC/settings.py
@@ -149,3 +149,24 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True  # Para desenvolvimento
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
